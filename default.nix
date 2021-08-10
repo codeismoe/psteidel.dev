8d7974801c48729eedad180cc752ae7137a679fd
Peter Steidel <psteidelprogramming@gmail.com>
2021-08-10 10:55:32 -0400

    Fix CI/CD using release.nix

--- default.nix
+++ default.nix
@@ -1,9 +1,7 @@
 {  pkgs ? import <nixpkgs> {} }:
 let
   myPackage = pkgs.haskellPackages.callCabal2nix "psteidel-dev" ./. {};
-  withTools = myPackage.overrideDerivation (
-    old: {
-      nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.lessc ];
-    }
-  );
+  withTools = myPackage.overrideDerivation (old: {
+    nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.lessc pkgs.netlify-cli];
+  });
 in withTools
